ffe39535e7b3eec4b162e5736111dd4b7dea505f
kwangmeng <kwangmengkenny@icloud.com>
2016-09-07 12:32:26 +0800

    Update db.php

--- samples/db.php
+++ samples/db.php
@@ -1,6 +1,10 @@
 <?php
 
+$dbuser = "PUT IN YOUR DB USERNAME";
+$dbname = "PUT IN YOUR DB NAME";
+$dbhost = "PUT IN YOUR DB HOSTING";
+$dbpass = "PUT IN YOUR DB PASSWORD";
 
-
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
 ?>
